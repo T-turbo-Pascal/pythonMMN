ea9b5d5e68382f3819be99d59c8d8d36812ce2bc
T-turbo-Pascal <70170383+T-turbo-Pascal@users.noreply.github.com>
2023-10-25 12:03:54 +0200

    some fix

--- Cvičenie 6/6.py
+++ Cvičenie 6/6.py
@@ -1,20 +1,24 @@
-def scitaj(a, b):
-    return a + b
+from random import *
 
 
-def kviz():
-    import random
-    while True:
-        a = random.randint(1, 10)
-        b = random.randint(1, 10)
-        vysledok = scitaj(a, b)
-        odpoved = input(str(a) + " + " + str(b) + " = ")
-        if odpoved == "":
-            break
-        if int(odpoved) == vysledok:
-            print("áno, je to tak")
-        else:
-            print("nie, skús znovu")
+def vymysli():
+    global a, b
+    a = randint(1, 10)
+    b = randint(1, 10)
+    print(f"{a} + {b} = ", end='')
 
 
-kviz()
+def opytajsa():
+    global vstup
+    vstup = input()
+
+
+vymysli()
+opytajsa()
+while vstup != '':
+    if a + b == int(vstup):
+        print('áno, je to tak')
+        vymysli()
+    else:
+        print('nie, skús znovu')
+    opytajsa()
